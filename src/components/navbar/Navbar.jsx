c6e8c0bb70ca3ba39d9d6fa2e10fc174d4a9b588
OgutuJoseph <onyango.joseph@yahoo.com>
2022-05-10 03:43:48 +0300

    Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,51 @@
 import './Navbar.scss';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
+import DarkModeOulinedIcon from '@mui/icons-material/DarkModeOutlined';
+import FullscreenExitOutlinedIcon from '@mui/icons-material/FullscreenExitOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import ChatBubbleOutlinedIcon from '@mui/icons-material/ChatBubbleOutlined';
+import ListOutlinedIcon from '@mui/icons-material/ListOutlined';
 
 function Navbar() {
     return (
-        <div>Navbar</div>
+        <div className='navbar'>
+            <div className='wrapper'>
+                <div className='search'>
+                    <input type='text' placeholder='Search...' />
+                    <SearchOutlinedIcon />
+                </div>
+                <div className='items'>
+                    <div className='item'>
+                        <LanguageOutlinedIcon className='icon' />
+                        English
+                    </div>
+                    <div className='item'>
+                        <DarkModeOulinedIcon className='icon' />
+                        English
+                    </div>
+                    <div className='item'>
+                        <FullscreenExitOutlinedIcon className='icon' />
+                        English
+                    </div>
+                    <div className='item'>
+                        <NotificationsNoneOutlinedIcon className='icon' />
+                        <div className='counter'>1</div>
+                    </div>
+                    <div className='item'>
+                        <ChatBubbleOutlinedIcon className='icon' />
+                        <div className='counter'>2</div>
+                    </div>
+                    <div className='item'>
+                        <ListOutlinedIcon className='icon' />
+                        English
+                    </div>
+                    <div className='item'>
+                        <img src='/images/avatar/avatar1.jpg' alt='' className='avatar' />
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
