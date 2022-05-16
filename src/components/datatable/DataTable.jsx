fe8376ea866a6c22a2e87648edee67e166ec36c2
OgutuJoseph <onyango.joseph@yahoo.com>
2022-05-16 16:03:43 +0300

    Delete using Context API

--- src/components/datatable/DataTable.jsx
+++ src/components/datatable/DataTable.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './DataTable.scss';
 import { DataGrid } from '@mui/x-data-grid';
 import { userColumns, userRows } from '../../datatablesource';
@@ -41,18 +42,24 @@ import { Link } from 'react-router-dom';
 
 const DataTable = () => {
 
+    const [data, setData] = useState(userRows);
+
+    const handleDelete = (id) => {
+        setData(data.filter(item => item.id !== id))
+    }
+
     const actionColumn = [
         { 
             field: 'action', 
             headerName: 'Action', 
             width: 200, 
-            renderCell: () => {
+            renderCell: (params) => {
                 return (
                     <div className='cellAction'>
                         <Link to='/users/test' style={{ textDecoration:'none' }}>
                             <div className='viewButton'>View</div>
                         </Link>
-                        <div className='deleteButton'>Delete</div>
+                        <div className='deleteButton' onClick={() => handleDelete(params.row.id)}>Delete</div>
                     </div>
                 )
             }
@@ -69,7 +76,7 @@ const DataTable = () => {
             </div>
             <DataGrid
                 className='datagrid'
-                rows={userRows}
+                rows={data}
                 columns={userColumns.concat(actionColumn)}
                 pageSize={9}
                 rowsPerPageOptions={[9]}
